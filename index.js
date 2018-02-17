f0197897002d7f4fd72a485be65de912ff7d9b06
Trevor Seitz <trevinla@gmail.com>
2018-02-17 17:17:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(katzDeliLine, customer) {
+  katzDeliLine.push(customer);
+  return (katzDeliLine.indexof(customer) + 1);
+  
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length < 1) {
+    return "There is nobody waiting to be served!";
+  } else {
+    var serving = katzDeliLine[1];
+    katzDeliLine.slice(1);
+    return serving;
+  }}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length < 1) {
+    return "The line is currently empty.";
+  } else {
+    return katzDeliLine;
+    
+  }
+}
