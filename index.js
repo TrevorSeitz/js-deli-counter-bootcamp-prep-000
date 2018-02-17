27d82b81e9dfdd0dda9b856d07a93c18aa7429a5
Trevor Seitz <trevinla@gmail.com>
2018-02-17 18:32:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function currentLine(line) {
 // for(var i=0; i<line.length; i++) {
 //   lineNamesandNumbers.push(i+1 + ". "+ line[i]);
 // }
-console.log("The line is currently: " + lineNamesandNumbers)
-return "The line is currently: " + lineNamesandNumbers.join(', ');
-}
+// console.log("The line is currently: " + lineNamesandNumbers)
+// return "The line is currently: " + lineNamesandNumbers.join(', ');
+// }
